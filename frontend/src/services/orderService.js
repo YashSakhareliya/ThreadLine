c7ce681e8353246772762abdb598e8791164523c
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-08-24 23:16:24 +0530

    update order service

--- frontend/src/services/orderService.js
+++ frontend/src/services/orderService.js
@@ -12,8 +12,19 @@ const createOrder = (orderData) => {
   return api.post('/orders', orderData);
 };
 
+const getShopOrders = (shopId, params = {}) => {
+  const queryString = new URLSearchParams(params).toString();
+  return api.get(`/shops/${shopId}/orders${queryString ? `?${queryString}` : ''}`);
+};
+
+const updateOrderStatus = (orderId, status) => {
+  return api.put(`/orders/${orderId}/status`, { status });
+};
+
 export default {
   getMyOrders,
   getOrderById,
-  createOrder
+  createOrder,
+  getShopOrders,
+  updateOrderStatus
 };
